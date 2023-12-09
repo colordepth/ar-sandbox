881f08f93171345fa79a21168f1416e5d7579af1
Deep Sharma <deepsharma.cs@gmail.com>
2023-12-09 20:13:37 +0530

    Convert tab to spaces

--- Assets/Scripts/Kinect/KinectInterface.cs
+++ Assets/Scripts/Kinect/KinectInterface.cs
@@ -6,11 +6,11 @@ using Windows.Kinect;
 /// </summary>
 public class KinectInterface : MonoBehaviour
 {
-	private KinectSensor kinect;
-	private DepthFrameReader depthReader;
+    private KinectSensor kinect;
+    private DepthFrameReader depthReader;
 
-	[HideInInspector]
-	public ushort[] DepthData;
+    [HideInInspector]
+    public ushort[] DepthData;
 
     public int FrameWidth { get; private set; }
 
@@ -19,59 +19,59 @@ public class KinectInterface : MonoBehaviour
     /// <summary>
     /// Polls the sensor for the next depth frame.
     /// </summary>
-	/// <returns>Whether new data is available.</returns>
+    /// <returns>Whether new data is available.</returns>
     private bool Poll()
-	{
-		if (depthReader != null)
-		{
-			DepthFrame depthframe = depthReader.AcquireLatestFrame();
-			if (depthframe != null)
-			{
-				depthframe.CopyFrameDataToArray(DepthData);
+    {
+        if (depthReader != null)
+        {
+            DepthFrame depthframe = depthReader.AcquireLatestFrame();
+            if (depthframe != null)
+            {
+                depthframe.CopyFrameDataToArray(DepthData);
                 depthframe.Dispose();
-				return true;
-			}
-		}
-		return false;
-	}
+                return true;
+            }
+        }
+        return false;
+    }
 
     /// <summary>
     /// Acquire kinect sensor and its depth sensor's resolution.
     /// </summary>
-    void Start ()
-	{
-		kinect = KinectSensor.GetDefault();
+    void Start()
+    {
+        kinect = KinectSensor.GetDefault();
 
-		if (kinect == null)
-		{
-			Debug.LogErrorFormat("KinectInterface: Kinect sensor not found!");
-			return;
-		}
+        if (kinect == null)
+        {
+            Debug.LogErrorFormat("KinectInterface: Kinect sensor not found!");
+            return;
+        }
 
-		depthReader = kinect.DepthFrameSource.OpenReader();
+        depthReader = kinect.DepthFrameSource.OpenReader();
 
-		FrameDescription depthFrameAttribs = depthReader.DepthFrameSource.FrameDescription;
-		this.FrameWidth = depthFrameAttribs.Width;
-		this.FrameHeight = depthFrameAttribs.Height;
+        FrameDescription depthFrameAttribs = depthReader.DepthFrameSource.FrameDescription;
+        this.FrameWidth = depthFrameAttribs.Width;
+        this.FrameHeight = depthFrameAttribs.Height;
 
-		// Allocate memory for depth image
-		this.DepthData = new ushort[depthFrameAttribs.LengthInPixels];
+        // Allocate memory for depth image
+        this.DepthData = new ushort[depthFrameAttribs.LengthInPixels];
     }
 
-	void Destroy()
-	{
-		if (depthReader != null)
-		{
-			depthReader.Dispose();
-			depthReader = null;
-		}
+    void Destroy()
+    {
+        if (depthReader != null)
+        {
+            depthReader.Dispose();
+            depthReader = null;
+        }
 
-		if (kinect != null)
-		{
-			if (kinect.IsOpen)
-				kinect.Close();
+        if (kinect != null)
+        {
+            if (kinect.IsOpen)
+                kinect.Close();
 
-			kinect = null;
-		}
-	}
+            kinect = null;
+        }
+    }
 }
