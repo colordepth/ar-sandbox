0cf4e3639937cba28a7b1fd6c8caf90447e8460a
Ambujbhaskar <bhaskarambuj7@gmail.com>
2024-01-27 19:23:32 +0530

    implement point rendering for markers, write basic structure for lines and polygons

---
+++ Assets/Scripts/Sandbox/Utils/Point.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point {
+	public Vector3 location;
+	public int size;
+	public Color32 color;
+	public string type;
+
+	public Point (Vector3 location, int size, Color32 color, string type) {
+		this.location = location;
+		this.size = size;
+		this.color = color;
+		this.type = type;
+	}
+}
